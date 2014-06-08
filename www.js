31672dbd0138413e6510a934c5b8f9508fb5bb11
peter <im7mortal@gmail.com>
2014-06-08 23:38:02 +0400

    hui

--- www.js
+++ www.js
@@ -35,6 +35,7 @@ function insertRating(id, amount, rights, res) {
 
 //        client.query("create table hsk (id char(64) PRIMARY KEY,amount char(64),rights char(64), date char(64))");
 //        res.end('create');
+    console.log(client.query("UPDATE hsk SET amount=$1, rights=$2 WHERE id=$3 ", [amount, rights, id]));
         client.query("UPDATE hsk SET amount=$1, rights=$2 WHERE id=$3 ", [amount, rights, id]);
 
         var query = client.query({
