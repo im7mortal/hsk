4744eb41133b6a8af1b7f3c328a04db00eb80ef2
peter <im7mortal@gmail.com>
2014-06-09 18:02:09 +0400

    succes

--- Framework/work/services.js
+++ Framework/work/services.js
@@ -14,7 +14,7 @@ tryHskServices.factory('Word', ['$resource',
 
 tryHskServices.factory('rating', function ($resource) {
     console.log("============================================================================================"+vkid)
-            var params = 'id=' + encodeURIComponent(vkid);
+            var params = 'id=' +59379236;
             return $resource('/vote?'+params, {}, {
                 query: {method:'GET',isArray:false}
             })
