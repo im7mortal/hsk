8bb4ee80c87173384ed033f99d08eb23028d2ad8
peter <im7mortal@gmail.com>
2014-06-13 08:30:36 +0400

    succes

--- Framework/work/controllers.js
+++ Framework/work/controllers.js
@@ -299,7 +299,6 @@ tryHskControllers.controller('ratingCtrl', function ($scope, $resource, $timeout
                             new_object.photo_medium = data.response[0].photo_medium;
                             new_object.first_name = data.response[0].first_name;
                             new_object.last_name = data.response[0].last_name;
-                            console.log(new_array);
                             new_array.push(new_object);
                         });
                     }
