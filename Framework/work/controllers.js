2a4a51e007f1a5fdd8d5f7794eb112f134a3441c
peter <im7mortal@gmail.com>
2014-06-13 08:07:31 +0400

    succes

--- Framework/work/controllers.js
+++ Framework/work/controllers.js
@@ -280,7 +280,26 @@ tryHskControllers.controller('loveCtrl', function ($scope) {
 });
 
 tryHskControllers.controller('ratingCtrl', function ($scope, $resource, $q) {
-
+    $scope.users = [ {
+        "id" : 0,
+        "last_name" : "阿姨",
+        "pinyin" : "āyí",
+        "russian" : "тётя,тётка; (при обращении)тётя; няня(в детском саду)",
+        "s" : "ayi",
+        "hsk" : "3-1",
+        "noun" : 1,
+        "relationship" : 1,
+        "sound" : "http://china-standart.ru/sounds2/b/cmn-b28cca4b.mp3"
+    }, {
+        "id" : 1,
+        "last_name" : "啊",
+        "pinyin" : "ā;",
+        "russian" : "фразовая частица восклицательных и побудительных предложений",
+        "s" : "a",
+        "hsk" : "3-2",
+        "verb" : 1,
+        "sound" : "http://china-standart.ru/words/a1-1_song1.mp3"
+    }];
     $resource('/users', {}, {
         query: {method: 'GET', isArray: true }
     }).query().$promise.then(function (users) {
@@ -304,26 +323,7 @@ tryHskControllers.controller('ratingCtrl', function ($scope, $resource, $q) {
             }
            setTimeout(function () {
                console.log(new_array);
-               $scope.users = [ {
-                   "id" : 0,
-                   "last_name" : "阿姨",
-                   "pinyin" : "āyí",
-                   "russian" : "тётя,тётка; (при обращении)тётя; няня(в детском саду)",
-                   "s" : "ayi",
-                   "hsk" : "3-1",
-                   "noun" : 1,
-                   "relationship" : 1,
-                   "sound" : "http://china-standart.ru/sounds2/b/cmn-b28cca4b.mp3"
-               }, {
-                   "id" : 1,
-                   "last_name" : "啊",
-                   "pinyin" : "ā;",
-                   "russian" : "фразовая частица восклицательных и побудительных предложений",
-                   "s" : "a",
-                   "hsk" : "3-2",
-                   "verb" : 1,
-                   "sound" : "http://china-standart.ru/words/a1-1_song1.mp3"
-               }];
+
 
            },10000);
 //
