124fd400acb0a693a49de0926f8cf0770cef7563
peter <im7mortal@gmail.com>
2014-06-13 08:54:02 +0400

    succes

--- Framework/work/controllers.js
+++ Framework/work/controllers.js
@@ -298,19 +298,22 @@ tryHskControllers.controller('ratingCtrl', function ($scope, $resource, $timeout
                     // Действия с полученными данными
                     console.log('ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
                     console.log(data.response);
-                    new_object.photo_medium = data.response[0].photo_medium;
-                    new_object.first_name = data.response[0].first_name;
-                    new_object.last_name = data.response[0].last_name;
-                    new_object.rating = users[0].rating;
+                                for (var i = 0; i < users.length; i++) {
+                (function () {
+                    new_object = null;
+                    new_object.photo_medium = data.response[i].photo_medium;
+                    new_object.first_name = data.response[i].first_name;
+                    new_object.last_name = data.response[i].last_name;
+                    new_object.rating = users[i].rating;
                     new_array.push(new_object);
+                })();
+            }
+console.log(new_array)
                 });
             }
 
 
-//            for (var i = 0; i < users.length; i++) {
-//                (function () {
-//                })();
-//            }
+
 
             $timeout(function() {
                 console.log(new_array);
