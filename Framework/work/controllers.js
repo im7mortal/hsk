1bff722a28220d268b6401c18f04b2b0d271687b
peter <im7mortal@gmail.com>
2014-06-13 08:51:42 +0400

    succes

--- Framework/work/controllers.js
+++ Framework/work/controllers.js
@@ -296,6 +296,7 @@ tryHskControllers.controller('ratingCtrl', function ($scope, $resource, $timeout
             } else {
                 VK.api("users.get", {user_ids: strt, fields: "photo_medium"}, function (data) {
                     // Действия с полученными данными
+                    console.log('ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
                     console.log(data.response);
                     new_object.photo_medium = data.response[0].photo_medium;
                     new_object.first_name = data.response[0].first_name;
