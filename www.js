2c441fd89b5f06df20d6cd4c8f10ede5db368bce
Peter Lozhkin <im7mortal@gmail.com>
2014-09-13 18:47:55 +0400

    noTask   поддержка facebook??

--- www.js
+++ www.js
@@ -8,6 +8,13 @@ var express = require("express")
 var strl;
 var port = Number(process.env.PORT || 5000);
 
+var sdk = require('facebook-node-sdk');
+
+var fb = new sdk({
+	appId: 635617779878563,
+	secret: '5d1fdfd275fef5abd6cbdfcb9c703616'
+}).setAccessToken(createdAccessToken);
+
 var conString = "postgres://kyhetrqttjglpi:949BScb2C_YjRZKFH2eA5ngz7-@ec2-54-235-245-180.compute-1.amazonaws.com:5432/d3i4729gmg7s1o";  //production
 //var conString = "postgres://sssr:hui@localhost/postgres";  //develop
 var index = fs.readFileSync('Framework/index.html');
